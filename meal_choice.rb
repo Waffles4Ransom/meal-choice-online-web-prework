6c379372f3fedfacf9d1f0c97d88ef72c0f5ffff
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-02 03:17:39 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,9 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 def meal_choice (choice="meat")
-  puts "I choose #{choice} please."
+  puts #{choice} 
 end
-
-meal_choice
-
-meal_choice ("vegan")
